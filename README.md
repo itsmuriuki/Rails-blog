8615427656340194282afa20272e4be6ebf804a2
Gerald Munyingi <grdmuriuki@gmail.com>
2017-06-08 10:33:45 +0000

    test

--- README.md
+++ README.md
@@ -25,3 +25,4 @@ Things you may want to cover:
 
 
 great to be here!
+just testing out git 
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,3 @@
-class Comment < ApplicationRecord
-  belongs_to :article
+class Comment < 
+    belongs_to :article
 end
